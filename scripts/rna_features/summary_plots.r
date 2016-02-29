450c0766cfa236162ded98a6e681e2078ff47c7f
James Allen <jallen@ebi.ac.uk>
2016-02-29 14:12:23 +0000

    Fixing bug with R library location; now passed to function and used explicitly

--- scripts/rna_features/summary_plots.r
+++ scripts/rna_features/summary_plots.r
@@ -1,27 +1,35 @@
-library(optparse)
-library(plyr)
-library(reshape)
+args = commandArgs(trailingOnly = TRUE)
+for(i in 1:length(args))
+{
+  if (args[i] == "-l" || args[i] == "--library") {
+    lib=args[i+1]
+  }
+}
+library(optparse, lib.loc=lib)
+library(plyr, lib.loc=lib)
+library(reshape, lib.loc=lib)
 
 option_list = list(
   make_option(c("-i", "--inputfile"), type="character", help="Input file summarising the alignments"),
   make_option(c("-e", "--evalue"), type="double", default=1e-06, help="E-value cut-off [default %default]"),
   make_option(c("-b", "--biotypesfile"), type="character", default="biotypes.svg", help="Output file for boxplot of total aligments per biotype [default %default]"),
   make_option(c("-d", "--distinctfile"), type="character", default="distinct.svg", help="Output file for bar chart of distinct models per species [default %default]"),
-  make_option(c("-c", "--plotcolour"), type="character", default="forestgreen", help="Colour for the plots [default %default]")
+  make_option(c("-c", "--plotcolour"), type="character", default="forestgreen", help="Colour for the plots [default %default]"),
+  make_option(c("-l", "--library"), type="character", default="R_lib", help="R library location")
 )
 parser = OptionParser(usage="%prog [options]", option_list=option_list)
-args = parse_args(parser)
+options = parse_args(parser, args=args)
 
-if (is.null(args$inputfile)) {
+if (is.null(options$inputfile)) {
   print("--inputfile is required")
   quit()
 }
 
-aln_summary = read.delim(args$inputfile, header=FALSE)
+aln_summary = read.delim(options$inputfile, header=FALSE)
 col_names = c('Evalue', 'Species', 'Biotype', 'Name')
 names(aln_summary) = col_names
 
-aln_filtered = subset(aln_summary, Evalue <= args$evalue | Evalue == 0)
+aln_filtered = subset(aln_summary, Evalue <= options$evalue | Evalue == 0)
 aln_filtered$Evalue = NULL
 
 # Biotypes boxplot
@@ -29,10 +37,10 @@ biotypes = cast(aln_filtered, Species ~ Biotype)
 biotypes$Species = NULL
 biotypes[biotypes==0] = NA
 
-svg(args$biotypesfile, width=ncol(biotypes)*0.75, height=6)
+svg(options$biotypesfile, width=ncol(biotypes)*0.75, height=6)
 par(las=2)
 par(mar=c(10, 5, 2, 2)+ 0.1)
-boxplot(biotypes, log="y", ylab="Total alignments", col=c(args$plotcolour))
+boxplot(biotypes, log="y", ylab="Total alignments", col=c(options$plotcolour))
 dev.off()
 
 
@@ -41,9 +49,9 @@ distinct_names = count(aln_filtered, vars = c("Species", "Name"))
 distinct_names$freq = NULL
 name_freqs = count(distinct_names, vars = c("Species"))
 
-svg(args$distinctfile, width=nrow(name_freqs)/3, height=6)
+svg(options$distinctfile, width=nrow(name_freqs)/3, height=6)
 par(las=2)
 par(mar=c(12, 5, 2, 2)+ 0.1)
 max_y = (ceiling(max(name_freqs$freq)/50))*50
-barplot(name_freqs$freq, ylab="Distinct Rfam models", ylim=c(0, max_y), names.arg=name_freqs$Species, col=c(args$plotcolour))
+barplot(name_freqs$freq, ylab="Distinct Rfam models", ylim=c(0, max_y), names.arg=name_freqs$Species, col=c(options$plotcolour))
 dev.off()
