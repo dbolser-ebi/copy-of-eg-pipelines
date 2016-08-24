5de2eac33d49acbd28e46cb2b66ab84951f55679
James Allen <jallen@ebi.ac.uk>
2016-08-24 13:50:50 +0100

    Adding RefSeq as a possible data source. Methods for unsetting, and later updating, the external_db release column.

--- modules/Bio/EnsEMBL/EGPipeline/BlastAlignment/FetchRefSeq.pm
+++ modules/Bio/EnsEMBL/EGPipeline/BlastAlignment/FetchRefSeq.pm
@@ -26,7 +26,7 @@ Bio::EnsEMBL::EGPipeline::BlastAlignment::FetchRefSeq
 
 =head1 DESCRIPTION
 
-Download RefSeq files from ftp site and convert into Fasta format.
+Download RefSeq files from ftp site and consolidate into a single fasta file.
 
 =head1 Author
 
@@ -38,50 +38,44 @@ package Bio::EnsEMBL::EGPipeline::BlastAlignment::FetchRefSeq;
 
 use strict;
 use warnings;
-use feature 'say';
-use base ('Bio::EnsEMBL::EGPipeline::Common::RunnableDB::Base');
+use base ('Bio::EnsEMBL::EGPipeline::BlastAlignment::FetchExternal');
 
-use Bio::SeqIO;
 use File::Basename qw(fileparse);
-use File::Copy qw(copy);
 use File::Path qw(make_path);
 use File::Spec::Functions qw(catdir);
-use Net::FTP;
-use URI;
+use IO::Uncompress::Gunzip qw(gunzip);
+use Path::Tiny qw(path);
 
 sub param_defaults {
   return {
-    'ebi_path'  => '/nfs/panda/ensemblgenomes/external/refseq',
-    'ftp_uri'   => 'ftp://ftp.ncbi.nlm.nih.gov/refseq/release',
-    'data_type' => 'protein',
+    'ebi_path'     => '/nfs/panda/ensemblgenomes/external/refseq',
+    'ftp_uri'      => 'ftp://ftp.ncbi.nlm.nih.gov/refseq/release',
+    'data_type'    => 'protein',
+    'file_varname' => 'refseq_fasta_file',
   };
 }
 
 sub fetch_input {
   my ($self) = @_;
-  my $out_dir            = $self->param_required('out_dir');
-  my $taxonomic_division = $self->param_required('taxonomic_division');
-  my $data_type          = $self->param_required('data_type');
-  my $species            = $self->param_required('species');
-  my $refseq_species     = $self->param('refseq_species');
+  my $out_dir         = $self->param_required('out_dir');
+  my $taxonomic_level = $self->param_required('taxonomic_level');
+  my $data_type       = $self->param_required('data_type');
   
   if (!-e $out_dir) {
     $self->warning("Output directory '$out_dir' does not exist. I shall create it.");
     make_path($out_dir) or $self->throw("Failed to create output directory '$out_dir'");
   }
   
-  if (! defined $refseq_species) {
-    $refseq_species = $species;
-    $self->param('refseq_species', $refseq_species);
-  }
+  $self->param('ebi_path', catdir($self->param('ebi_path'), $taxonomic_level));
+  $self->param('ftp_uri',  $self->param('ftp_uri') . "/$taxonomic_level");
   
   my $refseq_files;
   if ($data_type eq 'protein') {
-    $refseq_files = "$taxonomic_division.*.protein.faa.gz";
+    $refseq_files = "$taxonomic_level.*.protein.faa.gz";
   } else {
-    $refseq_files = "$taxonomic_division.*.rna.fna.gz";
+    $refseq_files = "$taxonomic_level.*.rna.fna.gz";
   }
-  my $output_file  = catdir($out_dir, "refseq_$refseq_species\_$data_type.fa");
+  my $output_file = catdir($out_dir, "refseq_$taxonomic_level\_$data_type.fa");
   
   $self->param('refseq_files', $refseq_files);
   $self->param('output_file', $output_file);
@@ -89,16 +83,13 @@ sub fetch_input {
 
 sub run {
   my ($self) = @_;
-  my $refseq_ebi_path    = $self->param_required('refseq_ebi_path');
-  my $refseq_ftp_uri     = $self->param_required('refseq_ftp_uri');
-  my $taxonomic_division = $self->param_required('taxonomic_division');
-  my $data_type          = $self->param_required('data_type');
-  my $out_dir            = $self->param_required('out_dir');
-  my $refseq_files       = $self->param_required('refseq_files');
-  my $output_file        = $self->param_required('output_file');
-  my $refseq_species     = $self->param_required('refseq_species');
+  my $ebi_path     = $self->param_required('ebi_path');
+  my $ftp_uri      = $self->param_required('ftp_uri');
+  my $out_dir      = $self->param_required('out_dir');
+  my $refseq_files = $self->param_required('refseq_files');
+  my $output_file  = $self->param_required('output_file');
   
-  my $ebi_files = catdir($refseq_ebi_path, $taxonomic_division, $refseq_files);
+  my $ebi_files = catdir($ebi_path, $refseq_files);
   
   # The trickiness here is that the refseq data is stored in multiple
   # files. We will iterate over them, appending to the output file;
@@ -119,142 +110,19 @@ sub run {
       push @local_files, $local_file;
     }
   } else {
-    my $uri = "$refseq_ftp_uri/$taxonomic_division";
-    my $ftp = $self->get_ftp($uri);
-    @local_files = @{ $self->fetch_ftp_file($ftp, $refseq_files, $out_dir) };
+    my $ftp = $self->get_ftp($ftp_uri);
+    @local_files = @{ $self->fetch_ftp_files($ftp, $refseq_files, $out_dir) };
   }
   
+  my $output = path($output_file);
   foreach my $local_file (@local_files) {
-    $self->extract_species($local_file, $output_file, $refseq_species, $data_type);
-  }
-}
-
-sub write_output {
-  my ($self) = @_;
-  
-  my $output_id = {
-    'db_fasta_file' => $self->param('output_file'),
-  };
-  $self->dataflow_output_id($output_id, 1);
-}
-
-sub fetch_ebi_file {
-  my ($self, $file, $local_file) = @_;
-
-  my $ebi_size = -s $file;
-  my $ebi_mdtm = (stat $file)[9];
-  
-  if (-e $local_file) {
-    my $local_size = -s $local_file;
-    my $local_mdtm = (stat $local_file)[9];
+    gunzip $local_file => "$local_file.tmp";
     
-    if ( ($ebi_size == $local_size) && ($ebi_mdtm == $local_mdtm) ) {
-      $self->warning("Using existing file '$local_file' with matching timestamp.");
-    } else {
-      copy($file, $local_file) or $self->throw("Failed to get '$file': $@");
-    }
-  } else {
-    copy($file, $local_file) or $self->throw("Failed to get '$file': $@");
-  }
-  
-  # Set the local timestamp to match the remote one.
-  utime $ebi_mdtm, $ebi_mdtm, $local_file;
-  
-  if (! -e $local_file) {
-    $self->throw("Failed to copy file '$file'.");
+    my $data = path("$local_file.tmp")->slurp;
+    $output->append($data);
+    
+    unlink "$local_file.tmp";
   }
 }
 
-sub get_ftp {
-  my ($self, $uri) = @_;
-  
-  my $ftp_uri = URI->new($uri);
-  my $ftp_host = $ftp_uri->host;
-  my $ftp_path = $ftp_uri->path;
-  
-  my $ftp = Net::FTP->new($ftp_host) or $self->throw("Failed to reach FTP host '$ftp_host': $@");
-  $ftp->login or $self->throw(printf("Anonymous FTP login failed: %s.\n", $ftp->message));
-  $ftp->cwd($ftp_path) or $self->throw(printf("Failed to change directory to '$ftp_path': %s.\n", $ftp->message));
-  $ftp->binary();
-  
-  return $ftp;
-}
-
-sub fetch_ftp_file {
-  my ($self, $ftp, $files_pattern, $out_dir) = @_;
-  
-  my @all_files = $ftp->ls();
-  my @local_files;
-  
-  my @files = grep { $_ =~ /$files_pattern/ } @all_files;
-  for my $file (@files) {
-    my $remote_size = $ftp->size($file);
-    my $remote_mdtm = $ftp->mdtm($file);
-    
-    my $local_file = catdir($out_dir, $file);
-    
-    if (-e $local_file) {
-      my $local_size = -s $local_file;
-      my $local_mdtm = (stat $local_file)[9];
-      
-      if ( ($remote_size == $local_size) && ($remote_mdtm == $local_mdtm) ) {
-        $self->warning("Using existing file '$local_file' with matching timestamp.");
-      } else {
-        $ftp->get($file, $local_file) or $self->throw("Failed to get '$file': $@");
-      }
-    } else {
-      $ftp->get($file, $local_file) or $self->throw("Failed to get '$file': $@");
-    }
-    
-    # Set the local timestamp to match the remote one.
-    utime $remote_mdtm, $remote_mdtm, $local_file;
-    
-    if (! -e $local_file) {
-      $self->throw("Failed to download file '$file'.");
-    } else {
-      push @local_files, $local_file;
-    }
-  }
-  
-  return \@local_files;
-}
-
-sub extract_species {
-  my ($self, $file, $output_file, $species, $data_type) = @_;
-  
-  my $seq_out = Bio::SeqIO->new(
-    -file   => '>>'.$output_file,
-    -format => 'fasta',
-  );
-  
-  $species =~ s/_/ /g;
-  $species =~ s/[A-Z]//g;
-  $species = ucfirst($species);
-  
-  # Don't forget these are gzipped files...
-  open(F, "gunzip -c $file |");
-  my $seq_in = Bio::SeqIO->new(
-    -fh     => \*F,
-    -format => 'fasta',
-  );
-  
-  while (my $inseq = $seq_in->next_seq) {
-    if ($self->species_match($inseq->desc, $species, $data_type)) {
-      my $display_id = $inseq->display_id;
-      $display_id =~ s/.*?([^\|]+)\|?$/$1/;
-      $inseq->display_id($display_id);
-      $seq_out->write_seq($inseq);
-    }
-  }
-}
-
-sub species_match {
-  my ($self, $desc, $species, $data_type) = @_;
-  
-  if ($data_type eq 'protein') {
-    return $desc =~ /\[$species\]/;
-  } else {
-    return $desc =~ /^$species/;
-  }
-}
 1;
--- modules/Bio/EnsEMBL/EGPipeline/ProteinFeaturesXref/addXref.pm
+++ modules/Bio/EnsEMBL/EGPipeline/ProteinFeaturesXref/addXref.pm
@@ -6,18 +6,14 @@ use warnings;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::DBEntry;
 
-use base ('Bio::EnsEMBL::EGPipeline::Common::RunnableDB::Base');
+use Time::Piece;
 
-sub param_defaults {
-  my ($self) = @_;
-  return {
-   'external_db' => 'Uniprot/SPTREMBL',
-  };
-}
+use base ('Bio::EnsEMBL::EGPipeline::Common::RunnableDB::Base');
 
 sub run {
   my ($self) = @_;
-  my $logic_name = $self->param_required('logic_name');
+  my $logic_name  = $self->param_required('logic_name');
+  my $external_db = $self->param_required('external_db');
   
   my $dba  = $self->core_dba();
   my $pfa  = $dba->get_adaptor('ProteinFeature');
@@ -27,14 +23,17 @@ sub run {
   my $analysis = $aa->fetch_by_logic_name($logic_name);
   my @ProteinFeaturesArray = @{ $pfa->fetch_all_by_logic_name($logic_name) };
   
+  $self->external_db_reset($dba, $external_db);
+  
   foreach my $feature (@ProteinFeaturesArray){
-    $self->add_xref($dbea, $feature, $analysis);
+    $self->add_xref($dbea, $feature, $analysis, $external_db);
   }
+  
+  $self->external_db_update($dba, $external_db);
 }
 
 sub add_xref {
-  my ($self, $dbea, $feature, $analysis) = @_;
-  my $external_db = $self->param_required('external_db');
+  my ($self, $dbea, $feature, $analysis, $external_db) = @_;
   my $hit_name = $feature->hseqname();
   
   my $xref = Bio::EnsEMBL::DBEntry->new
@@ -43,8 +42,30 @@ sub add_xref {
       -primary_id  => $hit_name,
       -display_id  => $hit_name,
     );
+  
   $xref->analysis($analysis);
-  $dbea->store($xref, $feature->translation_id(), 'Translation');
+  $dbea->store($xref, $feature->seqname(), 'Translation');
+}
+
+sub external_db_reset {
+  my ($self, $dba, $db_name) = @_;
+  
+  my $dbh = $dba->dbc->db_handle();
+  my $sql = "UPDATE external_db SET db_release = NULL WHERE db_name = ?;";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($db_name) or $self->throw("Failed to execute ($db_name): $sql");
+}
+
+sub external_db_update {
+  my ($self, $dba, $db_name) = @_;
+  
+  my $t = localtime;
+  
+  my $db_release = "EG Alignment Xref pipeline; ".$t->datetime;
+  my $dbh = $dba->dbc->db_handle();
+  my $sql = "UPDATE external_db SET db_release = ? WHERE db_name = ?;";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($db_release, $db_name) or $self->throw("Failed to execute ($db_release, $db_name): $sql");
 }
 
 1;
