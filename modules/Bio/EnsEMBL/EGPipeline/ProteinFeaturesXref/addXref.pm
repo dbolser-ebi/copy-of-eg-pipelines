5171b20bb21a615b8f9e2cfd70354341374d39bd
James Allen <jallen@ebi.ac.uk>
2016-09-07 11:02:41 +0100

    Refactoring and tidying code

--- modules/Bio/EnsEMBL/EGPipeline/ProteinFeaturesXref/addXref.pm
+++ modules/Bio/EnsEMBL/EGPipeline/ProteinFeaturesXref/addXref.pm
@@ -3,9 +3,6 @@ package Bio::EnsEMBL::EGPipeline::ProteinFeaturesXref::addXref;
 use strict;
 use warnings;
 
-use Data::Dumper;
-use Bio::EnsEMBL::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::DBEntry;
 use Bio::EnsEMBL::IdentityXref;
 use List::Util qw(min max);
 use Time::Piece;
@@ -23,13 +20,13 @@ sub run {
   my $dbea = $dba->get_adaptor('DBEntry');
 
   my $analysis = $aa->fetch_by_logic_name($logic_name);
-  my @ProteinFeaturesArray = @{ $pfa->fetch_all_by_logic_name($logic_name) };
+  my $protein_features = $pfa->fetch_all_by_logic_name($logic_name);
 
   $self->external_db_reset($dba, $external_db);
 
   my %protein_features;
 
-  foreach my $feature (@ProteinFeaturesArray) {
+  foreach my $feature (@$protein_features) {
     my $translation_id = $feature->seqname();
     my $hit_name       = $feature->hseqname();
     push @{$protein_features{$translation_id}{$hit_name}}, $feature;
@@ -38,12 +35,12 @@ sub run {
   $self->add_xrefs($dbea, $analysis, $external_db, \%protein_features);
 
   $self->external_db_update($dba, $external_db);
-  $self->delete_protein_features($dba,$logic_name);
+
+  #$self->delete_protein_features($dba, $logic_name);
 }
 
 sub add_xrefs {
   my ($self, $dbea, $analysis, $external_db, $protein_features) = @_;
-  my $ignore_release = 1;
 
   foreach my $translation_id (keys %$protein_features) {
     foreach my $hit_name (keys %{$$protein_features{$translation_id}}) {
@@ -51,58 +48,50 @@ sub add_xrefs {
 
       my @features = @{$$protein_features{$translation_id}{$hit_name}};
       if (scalar(@features) == 1) {
-        $xref = $self->add_xref($dbea, $features[0], $analysis, $external_db);
+        $xref = $self->add_xref($analysis, $external_db, $hit_name, $features[0]);
       } else {
-        $xref = $self->add_compound_xref($dbea, $analysis, $external_db, $hit_name, \@features);
+        $xref = $self->add_compound_xref($analysis, $external_db, $hit_name, \@features);
       }
 
-      $dbea->store($xref, $translation_id, 'Translation', $ignore_release);
+      $dbea->store($xref, $translation_id, 'Translation');
     }
   }
 }
 
 sub add_xref {
-  my ($self, $dbea, $feature, $analysis, $external_db) = @_;
+  my ($self, $analysis, $external_db, $hit_name, $feature) = @_;
 
-  my $hit_name = $feature->hseqname();
-  my $ensembl_start = $feature->start();
-  my $ensembl_end = $feature->end();
-  my $xref_start = $feature->hstart();
-  my $xref_end = $feature->hend();
-  my $xref_identity = $feature->percent_id();
-  my $evalue = $feature->p_value();
-  my $score = $feature->score();
-  my $coverage = $feature->coverage();
-  my $ensembl_identity = ($xref_end - $xref_start + 1)  * ($xref_identity) / ($ensembl_end - $ensembl_start +      1);
+  my $ensembl_length   = $feature->end - $feature->start + 1;
+  my $xref_length      = $feature->hend - $feature->hstart + 1;
+  my $xref_identity    = $feature->percent_id;
+  my $ensembl_identity = ($xref_length * $xref_identity) / $ensembl_length;
 
   my $xref = Bio::EnsEMBL::IdentityXref->new(
-    -XREF_IDENTITY    => $xref_identity,
-    -ENSEMBL_IDENTITY => $ensembl_identity,
-    -EVALUE           => $evalue,
-    -QUERY_START      => $xref_start,
-    -QUERY_END        => $xref_end,
-    -ENSEMBL_START    => $ensembl_start,
-    -ENSEMBL_END      => $ensembl_end,
-    -ADAPTOR          => $dbea,
-    -PRIMARY_ID       => $hit_name,
     -DBNAME           => $external_db,
+    -PRIMARY_ID       => $hit_name,
     -DISPLAY_ID       => $hit_name,
-    -SCORE            => $score,
+    -ENSEMBL_START    => $feature->start,
+    -ENSEMBL_END      => $feature->end,
+    -QUERY_START      => $feature->hstart,
+    -QUERY_END        => $feature->hend,
+    -ENSEMBL_IDENTITY => $ensembl_identity,
+    -XREF_IDENTITY    => $xref_identity,
+    -EVALUE           => $feature->p_value,
+    -SCORE            => $feature->score,
   );
-
   $xref->analysis($analysis);
 
   return $xref;
 }
 
 sub add_compound_xref {
-  my ($self, $dbea, $analysis, $external_db, $hit_name, $features) = @_;
+  my ($self, $analysis, $external_db, $hit_name, $features) = @_;
 
   my @ensembl_starts;
   my @ensembl_ends;
   my @xref_starts;
   my @xref_ends;
-  my ($ensembl_length, $xref_length, $xref_match);
+  my ($ensembl_length, $xref_length, $matches);
 
   foreach my $feature (@$features) {
     push @ensembl_starts, $feature->start;
@@ -110,31 +99,25 @@ sub add_compound_xref {
     push @xref_starts,    $feature->hstart;
     push @xref_ends,      $feature->hend;
 
-    $ensembl_length += $feature->end  - $feature->start  + 1;
-    $xref_length    += $feature->hend - $feature->hstart + 1;
-    $xref_match     += ($feature->hend - $feature->hstart + 1) * $feature->percent_id;
+    $ensembl_length +=  $feature->end  - $feature->start  + 1;
+    $xref_length    +=  $feature->hend - $feature->hstart + 1;
+    $matches        += ($feature->hend - $feature->hstart + 1) * $feature->percent_id;
   }
 
-  my $ensembl_start = min(@ensembl_starts);
-  my $ensembl_end = max(@ensembl_ends);
-  my $xref_start = min(@xref_starts);
-  my $xref_end = max(@xref_ends);
-  my $xref_identity = sprintf("%.2f", $xref_match/$xref_length);
-  my $ensembl_identity = sprintf("%.2f", $xref_match/$ensembl_length);
+  my $xref_identity    = sprintf("%.2f", $matches/$xref_length);
+  my $ensembl_identity = sprintf("%.2f", $matches/$ensembl_length);
 
   my $xref = Bio::EnsEMBL::IdentityXref->new(
-    -XREF_IDENTITY    => $xref_identity,
-    -ENSEMBL_IDENTITY => $ensembl_identity,
-    -QUERY_START      => $xref_start,
-    -QUERY_END        => $xref_end,
-    -ENSEMBL_START    => $ensembl_start,
-    -ENSEMBL_END      => $ensembl_end,
-    -ADAPTOR          => $dbea,
-    -PRIMARY_ID       => $hit_name,
     -DBNAME           => $external_db,
+    -PRIMARY_ID       => $hit_name,
     -DISPLAY_ID       => $hit_name,
+    -ENSEMBL_START    => min(@ensembl_starts),
+    -ENSEMBL_END      => max(@ensembl_ends),
+    -QUERY_START      => min(@xref_starts),
+    -QUERY_END        => max(@xref_ends),
+    -ENSEMBL_IDENTITY => $ensembl_identity,
+    -XREF_IDENTITY    => $xref_identity,
   );
-
   $xref->analysis($analysis);
 
   return $xref;
@@ -165,10 +148,12 @@ sub delete_protein_features {
   my ($self, $dba, $logic_name) = @_;
   my $dbh = $dba->dbc->db_handle();
 
-  my $sql = "DELETE protein_feature.* FROM protein_feature INNER JOIN analysis ON protein_feature.analysis_id = analysis.analysis_id WHERE analysis.logic_name = ?;";
+  my $sql =
+    "DELETE protein_feature.* FROM protein_feature INNER JOIN ".
+    "analysis ON protein_feature.analysis_id = analysis.analysis_id ".
+    "WHERE analysis.logic_name = ?;";
   my $sth = $dbh->prepare($sql);
   $sth->execute($logic_name) or $self->throw("Failed to execute ($logic_name): $sql");
-
 }
 
 1;
